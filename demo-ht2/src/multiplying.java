19637c4ac51834263c9222413038717722276ba4
Kateryna Lysenko <klysenko@playtika.com>
2018-03-02 16:17:03 +0200

    alignment

--- demo-ht2/src/multiplying.java
+++ demo-ht2/src/multiplying.java
@@ -6,8 +6,9 @@ public class multiplying {
         for (int j=1; j<=10; j++) {
 
             for (int i = 1; i <= 10; i++) {
-
-                System.out.print(i*j + " ");
+                    if (i*j<10) {
+                        System.out.print(i * j + "  ");
+                    }else System.out.print(i * j + " ");
             }
 
             System.out.println();
---
+++ demo-ht2/src/seven_numbers.java
@@ -0,0 +1,6 @@
+public class seven_numbers {
+
+    public static void main(String[] args) {
+        System.out.print("");
+    }
+}
